aea54d24546a225c8861217e2fc88ee3eecc81a8
PabloCasalino <pabloangelcasalino@gmail.com>
2024-05-09 14:09:25 -0300

    Validacion en campos de suscribirse.js

--- statics/script/suscribirse.js
+++ statics/script/suscribirse.js
@@ -1,16 +1,37 @@
-document.getElementById("form").addEventListener('change',function(event){
-  event.preventDefault();
-
-  var nombre = (document.getElementById("nombre").value);
-  var resultadoNombre = document.getElementById ("resultadoNombre");
-
-  let inputValue = nombre.trim();
-  let soloLetras = /^[a-zA-Z\s]*$/;
-
-  if(!soloLetras.test(inputValue)){
-    resultadoNombre.textContent = 'Por favor, ingrese un nombre válido';
-    resultadoNombre.style.color = 'red';
-  }
+  document.getElementById("form").addEventListener('change',function(event){
+    event.preventDefault();
+  
+    let nombre = document.getElementById("nombre").value;
+    let resultadoNombre = document.getElementById("resultadoNombre");
+    let apellido = document.getElementById("apellido").value;
+    let resultadoApellido = document.getElementById("resultadoApellido");
+    let submitButton = document.getElementById('submitButton');
+  
+    let inputValueNombre = nombre.trim();
+    let soloLetras = /^[a-zA-Z\s]*$/;
+  
+    let inputValueApellido = apellido.trim();
+    let soloLetrasApellido = /^[a-zA-Z\s]*$/;
+  
+    let nombreValido = soloLetras.test(inputValueNombre);
+    let apellidoValido = soloLetrasApellido.test(inputValueApellido);
+  
+    if (!nombreValido) {
+      resultadoNombre.textContent = 'Por favor, ingrese un nombre válido';
+      resultadoNombre.style.color = 'red';
+    } else {
+      resultadoNombre.textContent = '';
+    }
+  
+    if (!apellidoValido) {
+      resultadoApellido.textContent = 'Por favor, ingrese un apellido válido';
+      resultadoApellido.style.color = 'red';
+    } else {
+      resultadoApellido.textContent = '';
+    }
+  
+    submitButton.disabled = !(nombreValido && apellidoValido);
 });
+
 
 
